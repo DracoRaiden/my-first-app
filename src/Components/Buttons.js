63c25ffca1ddc224635e2e53ca3e8f1360f71979
Draco_Raiden <draco.raiden@gmail.com>
2025-07-22 15:41:42 +0500

    start

---
+++ src/Components/Buttons.js
@@ -0,0 +1,5 @@
+function Button({ btn }) {
+  return <button>{btn}</button>;
+}
+
+export default Button;
---
+++ src/Components/profile.js
@@ -0,0 +1,6 @@
+function profile({ name }) {
+  <img src="" alt=""></img>;
+  return <h1>{name}</h1>;
+  <p>Short Intro</p>;
+}
+export default profile;
