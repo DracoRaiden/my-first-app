db19dd5bb07ebd8a5667bcaf7cb19cb80b135363
Draco_Raiden <draco.raiden@gmail.com>
2025-07-25 14:15:26 +0500

    fixing dimensions

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import Header from "./Components/Header.js";
 import Hero from "./Components/Hero.js";
 import "./App.css";
+import Footer from "./Components/Footer.js";
 
 function App() {
   return (
     <div className="App">
       <div className="bg"></div>
       <div className="Header">
-        <Header />
-        <Hero />
+        <Header className="nav" />
+        <Hero className="hero" />
       </div>
       <div className="main"></div>
-      <div className="Footer"></div>
+      <div className="Footer">
+        <Footer />
+      </div>
     </div>
   );
 }
---
+++ src/Components/Footer.js
@@ -0,0 +1,13 @@
+import "../css/Footer.css";
+function Footer() {
+  return (
+    <footer>
+      <div className="footer-pannel1">
+        <a href="#">Back to Top</a>
+      </div>
+      <div className="footer-pannel2"></div>
+    </footer>
+  );
+}
+
+export default Footer;
